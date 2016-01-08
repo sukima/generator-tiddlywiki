b23aff044a8dfa6a7dd5af4aef4e79bafe1508ea
Devin Weaver <suki@tritarget.org>
2016-01-07 22:46:26 -0500

    Complete rewrite for version 3.0.0

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,37 +1,137 @@
 'use strict';
+var _ = require('lodash');
+var path = require('path');
+var util = require('util');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+
+var OUTPUT_PATH = 'wiki/output/*';
+
+function File(src) {
+  this.srcPath = src;
+  var parts = path.parse(src);
+  this.isTemplate = (parts.ext === '.ejs');
+  if (this.isTemplate) {
+    parts.base = parts.base.replace(parts.ext, '');
+    parts.ext = '';
+  }
+  parts.name = parts.name.replace(/^_/, '.');
+  parts.base = parts.base.replace(/^_/, '.');
+  this.destPath = path.format(parts);
+}
+File.prototype.process = function (ctx) {
+  var templatePath = ctx.templatePath(this.srcPath);
+  var destinationPath = ctx.destinationPath(this.destPath);
+  if (this.isTemplate) {
+    ctx.fs.copyTpl(templatePath, destinationPath, ctx.props);
+  } else {
+    ctx.fs.copy(templatePath, destinationPath);
+  }
+};
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
     var done = this.async();
 
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the prime ' + chalk.red('generator-tiddlywiki') + ' generator!'
-    ));
+    this.log(yosay(util.format(
+      'Welcome to the prime %s generator!', chalk.red('generator-tiddlywiki')
+    )));
+
+    function isDeployable(props) {
+      return props.deployChoice !== 'no deploy';
+    }
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name:     'name',
+      message:  'Wiki Name',
+      default:  path.basename(process.cwd()),
+      filter:   _.kebabCase,
+      validate: function (str) {
+        return str.length > 0;
+      }
+    }, {
+      name:    'description',
+      message: 'Description'
+    }, {
+      name:    'authorName',
+      message: 'Author\'s Name',
+      default: this.user.git.name(),
+      store:   true
+    }, {
+      name:    'authorEmail',
+      message: 'Author\'s Email',
+      default: this.user.git.email(),
+      store:   true
+    }, {
+      name:    'authorUrl',
+      message: 'Author\'s Homepage',
+      store:   true
+    }, {
+      name:    'babel',
+      type:    'confirm',
+      message: 'Use es2015 (precompiled with Babel)'
+    }, {
+      type:    'list',
+      name:    'deployChoice',
+      message: 'How will you deploy this?',
+      choices: ['no deploy', 'rsync', 'scp']
+    }, {
+      type:    'input',
+      name:    'deployUri',
+      message: 'Deploy URI (user@hostname.com:path/to/output)',
+      when:    isDeployable
     }];
 
     this.prompt(prompts, function (props) {
       this.props = props;
       // To access props later use this.props.someOption;
+      switch (props.deployChoice) {
+        case 'rsync':
+          this.props.deployCommand =
+            util.format('rsync -av %s %s', OUTPUT_PATH, props.deployUri);
+          break;
+        case 'scp':
+          this.props.deployCommand =
+            util.format('scp -r %s %s', OUTPUT_PATH, props.deployUri);
+          break;
+        default:
+          this.props.deployCommand = null;
+      }
 
       done();
     }.bind(this));
   },
 
+  configurating: function () {
+    var files = [
+      'README.md.ejs', '_jshintrc', '_gitignore', '_nvmrc', '_vimrc',
+      'tiddlers/_gitkeep', 'plugins/_gitkeep', 'themes/_gitkeep'
+    ];
+    if (this.props.babel) {
+      files.push('_babelrc');
+    }
+    files.forEach(function (src) {
+      new File(src).process(this);
+    }, this);
+  },
+
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    var files = [
+      'package.json.ejs', 'gulpfile.js.ejs', 'config/includes.json',
+      'config/libraries.json.ejs', 'config/tiddlywiki.info.ejs'
+    ];
+    if (this.props.babel) {
+      files.push(
+        'plugins/babel-polyfill/plugin.info',
+        'plugins/babel-polyfill/plugin.js.ejs',
+        'plugins/babel-polyfill/files/tiddlywiki.files'
+      );
+    }
+    files.forEach(function (src) {
+      new File(src).process(this);
+    }, this);
   },
 
   install: function () {
--- test/app.js
+++ test/app.js
@@ -1,19 +1,99 @@
 'use strict';
+var _ = require('lodash');
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
+var PROMPT_VALUES = {
+  name:        'test-name',
+  description: 'test-description',
+  authorName:  'test-author-name',
+  authorEmail: 'test-author-email',
+  authorUrl:   'test-author-url'
+};
+
+var HELPER_PATH = path.join(__dirname, '../generators/app');
+
 describe('generator-tiddlywiki:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
-      .on('end', done);
+  describe('no deploy', function () {
+    describe('with babel', function () {
+      before(function (done) {
+        helpers.run(HELPER_PATH)
+          .withPrompts(_.assign({}, PROMPT_VALUES, {
+            babel:        true,
+            deployChoice: 'no deploy'
+          }))
+          .on('end', done);
+      });
+
+      it('creates files', function () {
+        assert.file([
+          'README.md', '.babelrc', '.jshintrc', '.gitignore', '.nvmrc', '.vimrc',
+          'package.json', 'gulpfile.js', 'config/includes.json',
+          'config/libraries.json', 'config/tiddlywiki.info', 'tiddlers/.gitkeep',
+          'themes/.gitkeep', 'plugins/babel-polyfill/plugin.info',
+          'plugins/babel-polyfill/plugin.js',
+          'plugins/babel-polyfill/files/tiddlywiki.files'
+        ]);
+      });
+
+      it('does not include a deploy command', function () {
+        assert.noFileContent('package.json', new RegExp('"deploy":'));
+      });
+    });
+
+    describe('without babel', function () {
+      before(function (done) {
+        helpers.run(HELPER_PATH)
+          .withPrompts(_.assign({}, PROMPT_VALUES, {
+            babel:        false,
+            deployChoice: 'no deploy'
+          }))
+          .on('end', done);
+      });
+
+      it('does not include babel support files', function () {
+        assert.noFile([
+          '.babelrc', 'plugins/babel-polyfill/plugin.info',
+          'plugins/babel-polyfill/plugin.js',
+          'plugins/babel-polyfill/files/tiddlywiki.files'
+        ]);
+        assert.noFileContent('package.json', /babel/);
+        assert.noFileContent('gulpfile.js', /babel/);
+        assert.noFileContent('config/libraries.json', /babel/);
+      });
+    });
   });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+  describe('deploy rsync', function () {
+    before(function (done) {
+      helpers.run(HELPER_PATH)
+        .withPrompts(_.assign({}, PROMPT_VALUES, {
+          babel:        false,
+          deployChoice: 'rsync',
+          deployUri:    'foobar@example.com:path/to/output'
+        }))
+        .on('end', done);
+    });
+
+    it('includes rsync deply command', function () {
+      assert.fileContent('package.json', new RegExp('rsync.*foobar@example.com:path/to/output'));
+    });
+  });
+
+  describe('deploy scp', function () {
+    before(function (done) {
+      helpers.run(HELPER_PATH)
+        .withPrompts(_.assign({}, PROMPT_VALUES, {
+          babel:        false,
+          deployChoice: 'scp',
+          deployUri:    'foobar@example.com:path/to/output'
+        }))
+        .on('end', done);
+    });
+
+    it('includes scp deply command', function () {
+      assert.fileContent('package.json', new RegExp('scp.*foobar@example.com:path/to/output'));
+    });
   });
 });
